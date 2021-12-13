0f58bb683a5b9cc2bcc8cbd6be311e7cae139af8
JP3Pe <rlaworhkd430430@gmail.com>
2021-12-13 13:20:06 +0900

    add: 여행 모델 내부에 작성자 관련 FK 추가

--- trip/models.py
+++ trip/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -5,7 +6,7 @@ class Trip(models.Model):
     title = models.CharField(max_length=50)
     explanation = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    author = models.CharField(max_length=50)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
