528ccca758c215df67aea1dc816663798c892c30
JP3Pe <45834750+JP3Pe@users.noreply.github.com>
2021-12-13 13:24:57 +0900

    여행 게시글에 유저 관련 FK 관계 추가 (#5)
    * refactor: Trip model 내부의 컬럼명 변경
    
    * add: 여행 모델 내부에 작성자 관련 FK 추가

--- trip/models.py
+++ trip/models.py
@@ -1,11 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class Trip(models.Model):
     title = models.CharField(max_length=50)
-    content = models.TextField()
+    explanation = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    author = models.CharField(max_length=50)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
