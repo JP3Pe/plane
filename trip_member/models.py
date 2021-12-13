1900e85f187b944c5afe374645cb0859c492c09c
JP3Pe <rlaworhkd430430@gmail.com>
2021-12-13 13:07:21 +0900

    add: trip_member 모델 추가

--- trip_member/models.py
+++ trip_member/models.py
@@ -1,1 +1,15 @@
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+
+from trip.models import Trip
+
+
+class TripMember(models.Model):
+    member = models.ForeignKey(User, on_delete=models.CASCADE)
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('member', 'trip')
+
+    def __str__(self):
+        return f'여행 제목: {self.trip}, 여행 멤버: {self.member}'
