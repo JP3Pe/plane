aed60f3f4be320400699345bdd673aa40ea7ac45
JP3Pe <45834750+JP3Pe@users.noreply.github.com>
2021-12-14 20:33:04 +0900

    로그아웃 기능 추가 (#8)

--- user/urls.py
+++ user/urls.py
@@ -3,9 +3,8 @@ from django.urls import path
 from user import views
 
 urlpatterns = [
-    # TODO: Active links below
     path('', views.sign_in, name='sign-in'),
     path('sign-in', views.sign_in, name='sign-in'),
     path('sign-up', views.sign_up, name='sign-up'),
-    # path('logout', views.logout),
+    path('logout', views.logout, name='log-out'),
 ]
